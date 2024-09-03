c583ec5338eac30b51a9a9c981405950244c6f88
Andrew Rivett <veggiefrog@gmail.com>
2024-09-03 17:15:23 -0400

    Removed forced COLLATION from queries

--- public/linkSearchField.php
+++ public/linkSearchField.php
@@ -49,7 +49,7 @@ function ciniki_links_linkSearchField($ciniki) {
             . "AND " . $args['field'] . " <> '' "
             . ") "
         . "";
-    $strsql .= "ORDER BY " . $args['field'] . " COLLATE latin1_general_cs "
+    $strsql .= "ORDER BY " . $args['field'] . " "
         . "";
     if( isset($args['limit']) && $args['limit'] != '' && $args['limit'] > 0 ) {
         $strsql .= "LIMIT " . ciniki_core_dbQuote($ciniki, $args['limit']) . " ";
